84dc3137505c879637d4e3dd837e674d2b6e2792
danjiker1 <daniil.egorov2003@mail.ru>
2026-08-04 22:42:14 +0300

    Add application menu

--- task-manager/src/main/java/org/example/Main.java
+++ task-manager/src/main/java/org/example/Main.java
@@ -3,6 +3,12 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("--- Менеджер задач ---");
+        System.out.println("1. Добавить задачу");
+        System.out.println("2. Показать задачи");
+        System.out.println("3. Удалить задачу");
+        System.out.println("0. Выход");
+
         addTask();
         showTask();
         deleteTask();
